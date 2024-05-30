152482cb060c4ca36ea5a9b7e36edfb6a1aed8c3
hao <themesflatc10@gmail.com>
2024-05-30 09:39:48 +0700

    update

--- wp-content/plugins/post-duplicator/includes/hooks.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Mtphr\PostDuplicator\Hooks;
-
-// Disable WC product review count
-add_filter( 'mtphr_post_duplicator_meta__wc_review_count_enabled', '__return_false' );
--- wp-content/themes/wiatech/tpl/entry-content/entry-content-readmore.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package xstar
- */
-
-if ( get_post_format() != 'quote' ) {
-echo '<div class="themesflat-button-container"><a class="themesflat-btn-blog" href="'.get_the_permalink().'" rel="nofollow">'.themesflat_get_opt( 'blog_archive_readmore_text').'</a></div>';
-}
